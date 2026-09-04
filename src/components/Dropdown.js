282f6996e874948d88276ad8a51dcf240eff1077
Belal-said <belalelkholy33@gmail.com>
2026-09-04 11:32:58 -0700

    styling dropdown

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,27 +1,46 @@
-import { useState } from 'react'
+import { useState } from "react";
+import { GoChevronDown } from 'react-icons/go'
 
 function Dropdown({ options, onChange, value }) {
-    const [isOpen, SetIsOpen] = useState(false)
+    const [isOpen, SetIsOpen] = useState(false);
 
     const handleClick = () => {
-        SetIsOpen(!isOpen)
-    }
+        SetIsOpen(!isOpen);
+    };
 
     const handleOptionClick = (option) => {
         // close dropdown
-        SetIsOpen(false)
+        SetIsOpen(false);
         // what option did the user clicked on
-        onChange(option)
-    }
+        onChange(option);
+    };
 
     const renderedOptions = options.map((option) => {
-        return <div onClick={() => handleOptionClick(option)} key={option.value}>{option.label}</div>;
+        return (
+            <div
+                className="hover:bg-sky-100 rounded cursor-pointer p-1"
+                onClick={() => handleOptionClick(option)}
+                key={option.value}
+            >
+                {option.label}
+            </div>
+        );
     });
 
     return (
-        <div>
-            <div onClick={handleClick}>{value?.label || 'Select...'}</div>
-            {isOpen && <div>{renderedOptions}</div>}
+        <div className="w-48 relative">
+            <div
+                className="flex justify-between items-center cursor-pointer rounded border p-3 shadow bg-white w-full"
+                onClick={handleClick}
+            >
+                {value?.label || "Select..."}
+                <GoChevronDown />
+            </div>
+            {isOpen && (
+                <div className="absolute top-full border rounded p-3 shadow bg-white w-full">
+                    {renderedOptions}
+                </div>
+            )}
         </div>
     );
 }
